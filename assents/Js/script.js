81b27994e09eb0cc44838c8328da911fbf3cc93b
YanCarvalhoo <devyancarvalho15@gmail.com>
2025-04-04 14:37:31 -0300

    cento5

---
+++ assents/Js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+    strings: [
+        '<span style="color: white;">Frontend Developer</span>',
+        '<span style="color: white;">Programmer</span>',
+        '<span style="color: white;">Web Developer</span>'
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
